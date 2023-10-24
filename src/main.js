f0d185fe67ec9ea665c601abb2407839798759fe
ivanov9826 <ivanov9826@gmail.com>
2023-10-24 20:05:21 +0300

    Add bootstrap to main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
-import "./style.css";
 import App from "./App.vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
 import router from "@/router/index.js";
 
 const app = createApp(App);
