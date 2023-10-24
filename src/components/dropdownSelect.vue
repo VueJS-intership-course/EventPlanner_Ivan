45339800fc85441acf3e17062d31bfa7e7b63e3e
ivanov9826 <ivanov9826@gmail.com>
2023-10-24 21:44:28 +0300

    Add search dropdown with all timezones in register

---
+++ src/components/dropdownSelect.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <v-select v-model="selectedValue" :options="options"></v-select>
+    <p>Selected Value: {{ selectedValue }}</p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import vSelect from "vue-select";
+import "vue-select/dist/vue-select.css";
+import moment from "moment-timezone";
+
+const options = moment.tz.names();
+
+const selectedValue = ref(null);
+</script>
--- src/pages/RegisterPage/RegisterPage.vue
+++ src/pages/RegisterPage/RegisterPage.vue
@@ -1,8 +1,53 @@
 <template>
-  <h1>Register Page</h1>
-  <div></div>
+  <div class="d-flex flex-column p-2 main-container">
+    <h1>Register Page</h1>
+
+    <form>
+      <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">Email address:</label>
+        <input
+          type="email"
+          class="form-control"
+          id="exampleInputEmail1"
+          aria-describedby="emailHelp"
+        />
+        <div id="emailHelp" class="form-text">
+          We'll never share your email with anyone else.
+        </div>
+      </div>
+      <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">Password:</label>
+        <input type="password" class="form-control" />
+      </div>
+      <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">Repeat Password:</label>
+        <input type="password" class="form-control" />
+      </div>
+      <div class="mb-3">
+        <label for="timezone">City or State</label>
+        <DropdownSelect />
+      </div>
+      <div class="mb-3 form-check">
+        <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref, onMounted } from "vue";
+import DropdownSelect from "../../components/dropdownSelect.vue";
 
-<style lang="scss" scoped></style>
+const selectedValue = ref(null);
+
+// Initialize Select2 when the component is mounted
+</script>
+
+<style lang="scss" scoped>
+.main-container {
+  max-width: 30rem;
+  margin: auto;
+}
+</style>
